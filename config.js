20aa1456bb7dc429212ef9d6417e934c8f764c4d
Bankai <sarkarka2005@gmail.com>
2024-07-27 18:38:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.oname = author
 global.bname = botname
 
 //Extra Shortcuts
-global.smlink = process.env.SOCIAL_MEDIA_LINK || 'https://instagram.com/Bankai77'
+global.smlink = process.env.SOCIAL_MEDIA_LINK || 'https://instagram.com/neyxi.tt'
 global.gclink = process.env.GROUP_LINK || 'https://chat.whatsapp.com/LHfJmKoYVJp1gDX58P55CY'
  
 //Apikeys
