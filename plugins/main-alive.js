e6b016dc3bd7f917e987926187c778dfe6fdac22
Bankai <sarkarka2005@gmail.com>
2024-07-29 12:39:42 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://drive.google.com/file/d/1pCm7rFqF46dAF3V2oqWmleA7MaNlfAuY/view?usp=sharing'
+  var vn = 'https://eu-central.storage.cloudconvert.com/tasks/f3e46cd3-43a6-4938-b61c-91384eebf407/Sugarcrush2.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20240729%2Ffra%2Fs3%2Faws4_request&X-Amz-Date=20240729T070834Z&X-Amz-Expires=86400&X-Amz-Signature=2266fd90052cafbaf9ef1d4f96c7ff49b850ad241d127bacfeecc53d5dcfe4a2&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D%22Sugarcrush2.mp3%22&response-content-type=audio%2Fmpeg&x-id=GetObject'
   let url = 'https://github.com/Bankai77/KITAGAWA-MD'
   let murl = 'https://youtu.be/Z8lRhKH6ClE?si=U5oEtIG2vD1PWNZt'
   let img = 'https://images3.alphacoders.com/120/thumb-1920-1203462.jpg'
